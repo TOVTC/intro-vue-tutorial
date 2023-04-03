cb2764b2ca25cb83a9a3acfde5349b2b0deae89e
TOVTC <veronica.tc.to@outlook.com>
2023-04-03 10:15:52 -0400

    creating the vue app

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-const product = 'Socks'
+// this tutorial uses CDN to import Vue
+// create new instance of Vue app, passing in mandatory "Options object"
+// Options object adds additional properties to configure the application (must be included, but can be empty)
+const app = Vue.createApp({
+    // data option returns object with app data
+    data() {
+        // 
+        return {
+            product: "Socks",
+            description: "A pair of socks"
+        }
+    }
+})
