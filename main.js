c17b326d36d355703f9ce367700af5a39afb5623
TOVTC <veronica.tc.to@outlook.com>
2023-04-03 12:03:24 -0400

    starter code

--- main.js
+++ main.js
@@ -3,8 +3,9 @@ const app = Vue.createApp({
         return {
             cart:0,
             product: 'Socks',
+            brand: 'Vue Mastery',
             image: './assets/images/socks_blue.jpg',
-            inStock: true,
+            inStock: false,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
               { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
@@ -12,20 +13,12 @@ const app = Vue.createApp({
             ]
         }
     },
-    // add a list of methods to the app
     methods: {
         addToCart() {
-            // refers to the cart in this instance of our Vue app
             this.cart += 1
         },
-            // refers to the image currently being displayed in our Vue app instance
         updateImage(variantImage) {
             this.image = variantImage
-        },
-        removeFromCart() {
-            if (this.cart > 0) {
-                this.cart -= 1
-            }
         }
     }
 })
