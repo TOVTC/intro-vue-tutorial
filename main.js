92c21726510317a66f72f5092579980014c88efe
TOVTC <veronica.tc.to@outlook.com>
2023-04-03 16:12:09 -0400

    starter code

--- main.js
+++ main.js
@@ -1,15 +1,14 @@
 const app = Vue.createApp({
     data() {
         return {
-            cart:0,
+            cart: 0,
             product: 'Socks',
             brand: 'Vue Mastery',
-            // the selectedVariant property will be updated based on the index being hovered on
             selectedVariant: 0,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-              { id: 2234, color: 'green', onSale: false, image: './assets/images/socks_green.jpg', quantity: 50 },
-              { id: 2235, color: 'blue', onSale: true, image: './assets/images/socks_blue.jpg', quantity: 0 },
+              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
+              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 },
             ]
         }
     },
@@ -26,18 +25,10 @@ const app = Vue.createApp({
             return this.brand + ' ' + this.product
         },
         image() {
-            // selectedVariant targets the index being hovered on
             return this.variants[this.selectedVariant].image
         },
         inStock() {
-            // selectedVariant targets the index being hovered on
             return this.variants[this.selectedVariant].quantity
         },
-        onSale() {
-            if (this.variants[this.selectedVariant].onSale) {
-                return this.title + ' is on sale!'
-            }
-            return ''
-        }
     }
 })
