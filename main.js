2f1829c9bedf84ddf421fd585dbb6ead484eb8c4
TOVTC <veronica.tc.to@outlook.com>
2023-04-03 10:20:16 -0400

    starter code

--- main.js
+++ main.js
@@ -1,13 +1,7 @@
-// this tutorial uses CDN to import Vue
-// create new instance of Vue app, passing in mandatory "Options object"
-// Options object adds additional properties to configure the application (must be included, but can be empty)
 const app = Vue.createApp({
-    // data option returns object with app data
     data() {
-        // 
         return {
-            product: "Socks",
-            description: "A pair of socks"
+            product: 'Socks'
         }
     }
 })
