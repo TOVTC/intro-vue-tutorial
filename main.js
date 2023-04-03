cab8dd4c4aea8c758fedaabe3f2327d61a78f1d7
TOVTC <veronica.tc.to@outlook.com>
2023-04-03 10:38:55 -0400

    starter code

--- main.js
+++ main.js
@@ -2,9 +2,7 @@ const app = Vue.createApp({
     data() {
         return {
             product: 'Socks',
-            // this path will be imported into html
-            image: './assets/images/socks_green.jpg',
-            url: "https://vuejs.org/guide/introduction.html"
+            image: './assets/images/socks_blue.jpg'
         }
     }
 })
